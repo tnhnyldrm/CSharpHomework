d5fe38cc86522060c21aa5f410ea36375fcb3034
tnhnyldrm <46500254+tnhnyldrm@users.noreply.github.com>
2023-10-24 16:48:17 +0300

    method overload

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+//Add();
+//Add();
+//Add();
+//var result=Add2(20);
+
+/*
+int number1 = 20;
+int number2 = 100; //değer tipler
+var result2 = Add3(ref number1,number2);
+//ref referans tip methodta değişirse burda da değişir
+//out değer atamadan gönderilebilir, baştan set etme yok
+
+Console.WriteLine(result2);
+Console.WriteLine(number1);*/
+
+Console.WriteLine(Multiply(2, 4));
+Console.WriteLine(Multiply(2, 4, 5));
+
+static void Add()
+{
+    Console.WriteLine("Added!");
+}
+
+//default değerler her zaman en sonda olmalı
+static int Add2(int number1, int number2=30)
+{
+    var result = number1 + number2;
+    return result;
+}
+
+static int Add3(ref int number1, int number2)
+{
+    number1 = 30;
+    return number1 + number2;
+}
+
+static int Multiply(int number1, int number2)
+{
+    return number1 * number2;
+}
+
+//main içinde olduğu için hata veriyor
+/*
+static int Multiply(int number1, int number2, int number3)
+{
+    return number1 * number2 * number3;
+}*/
