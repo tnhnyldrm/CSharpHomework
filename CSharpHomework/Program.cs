e0df8d6ce701fc23af9be8e79e0cc95a64708f87
tnhnyldrm <46500254+tnhnyldrm@users.noreply.github.com>
2023-10-24 15:05:28 +0300

    short veri tipi

--- CSharpHomework/Program.cs
+++ CSharpHomework/Program.cs
@@ -1,4 +1,5 @@
 ﻿
+short number3 = 32767; //16bitlik yer kaplar
 int number1 = 2147483647; //32bitlik yer kaplar
 long number2 = 9223372036854775807; //64bitlik yer kaplar
 Console.WriteLine("number1 is {0}", number1);
