9e4a243d66d0ba656af243899e1d463fb625ba15
tnhnyldrm <46500254+tnhnyldrm@users.noreply.github.com>
2023-10-24 15:06:49 +0300

    byte veri tipi

--- CSharpHomework/Program.cs
+++ CSharpHomework/Program.cs
@@ -1,6 +1,9 @@
 ﻿
+byte number4 = 255; //8bitlik yer kaplar 
 short number3 = 32767; //16bitlik yer kaplar
 int number1 = 2147483647; //32bitlik yer kaplar
 long number2 = 9223372036854775807; //64bitlik yer kaplar
 Console.WriteLine("number1 is {0}", number1);
 Console.WriteLine("number2 is {0}", number2);
+Console.WriteLine("number3 is {0}", number3);
+Console.WriteLine("number4 is {0}", number4);
