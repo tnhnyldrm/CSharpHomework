01d105cd3e83fffe06fd2532910abbdecb79223c
tnhnyldrm <46500254+tnhnyldrm@users.noreply.github.com>
2023-10-24 21:00:25 +0300

    arrays final

---
+++ Arrays/Program.cs
@@ -0,0 +1,27 @@
+﻿
+string[] students = new string[3] { "engin", "derin", "salih" };
+
+string[] students2 = { "engin", "derin", "salih" };
+
+foreach (var student in students)
+{
+    Console.WriteLine(student); 
+}
+
+string[,] regions = new string[5,3]
+{
+    { "istanbul","izmit","balıkesir" },
+    { "ankara","konya","kırıkale" },
+    { "antalya","adana","mersin" },
+    { "rize","trabzon","samsun" },
+    { "izmir","muğla","manisa" }
+};
+
+for (int i = 0; i <= regions.GetUpperBound(0); i++)
+{
+    for (int j = 0; j <= regions.GetUpperBound(1); j++)
+    {
+        Console.WriteLine(regions[i, j]);
+    }
+    Console.WriteLine("*****************");
+}
