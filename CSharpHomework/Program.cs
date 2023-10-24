3b9749b4261f942b4e8d4475705231bdc4c7d528
tnhnyldrm <46500254+tnhnyldrm@users.noreply.github.com>
2023-10-24 15:04:24 +0300

    long veri tipi

--- CSharpHomework/Program.cs
+++ CSharpHomework/Program.cs
@@ -1,3 +1,5 @@
 ﻿
-int number1 = 2147483647;
+int number1 = 2147483647; //32bitlik yer kaplar
+long number2 = 9223372036854775807; //64bitlik yer kaplar
 Console.WriteLine("number1 is {0}", number1);
+Console.WriteLine("number2 is {0}", number2);
