072495db1bc3399b8f8d40d2b75b48acf8de5030
tnhnyldrm <46500254+tnhnyldrm@users.noreply.github.com>
2023-10-26 15:46:36 +0300

    Interfaces

---
+++ Interfaces/Program.cs
@@ -0,0 +1,93 @@
+﻿namespace Interfaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //InterfacesIntro();
+            //Demo();
+
+            ICustomerDal[] customerDals = new ICustomerDal[3]
+            {
+                new SqlServerCustomerDal(),
+                new OracleCustomerDal(),
+                new MySqlCustomerDal()
+            };
+
+            foreach (var customerDal in customerDals)
+            {
+                customerDal.Add();
+            }
+
+        }
+        private static void Demo()
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(new SqlServerCustomerDal());
+        }
+        private static void InterfacesIntro()
+        {
+            PersonManager manager = new PersonManager();
+            Customer customer = new Customer
+            {
+                Id = 1,
+                FirstName = "engin",
+                LastName = "demirog",
+                Address = "ankara"
+            };
+
+            Student student = new Student
+            {
+                Id = 1,
+                FirstName = "derin",
+                LastName = "demirog",
+                Departmant = "cs"
+            };
+
+            manager.Add(student);
+            manager.Add(customer);
+        }
+    }
+
+    interface IPerson
+    {
+        int Id { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+    }
+
+    class Customer : IPerson
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string Address { get; set; }
+    }
+
+    class Student : IPerson
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string Departmant { get; set; }
+    }
+
+    class Worker : IPerson
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string Departmant { get; set; }
+    }
+
+    class PersonManager
+    {
+        public void Add(IPerson person)
+        {
+            Console.WriteLine(person.FirstName);
+        }
+    }
+}
