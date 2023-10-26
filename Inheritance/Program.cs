d28a5518a7ede7bbc165f9b444d257bc361ccb5a
tnhnyldrm <46500254+tnhnyldrm@users.noreply.github.com>
2023-10-26 16:06:21 +0300

    Inheritance

---
+++ Inheritance/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] persons = new Person[3]
+            {
+                new Customer
+                {
+                    FirstName="engin"
+                },
+                new Student
+                {
+                    FirstName="derin"
+                },
+                new Person
+                {
+                    FirstName="salih"
+                }
+            };
+
+            foreach (Person person in persons)
+            {
+                Console.WriteLine(person.FirstName);
+            }
+        }
+    }
+
+    class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    class Customer:Person 
+    {
+        public string City { get; set; }
+    }
+
+    class Student: Person
+    {
+        public string Department { get; set; }
+    }
+}
